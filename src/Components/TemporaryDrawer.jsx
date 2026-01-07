bf39738e4c04d21587249d160dae2af1ad565481
jasur737 <baxtiyorovjasur728@gmail.com>
2026-01-07 15:06:32 +0500

    :)

--- src/Components/TemporaryDrawer.jsx
+++ src/Components/TemporaryDrawer.jsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = React.useState(false);
@@ -19,21 +19,28 @@ export default function TemporaryDrawer() {
   };
 
   const DrawerList = (
-    <Box sx={{ width: 1730 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+    <Box
+      sx={{ width: 1730, color: "white", }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+    >
+      <List className="flex flex-col items-center p-8 bg-[#333333] h-205">
+        <div>
+          <img src="src/assets/logo.png" alt="" />
+        </div>
+        {["Меню", "Доставка", "Оплата"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
-      <Divider />
-      <List>
+      {/* <Divider /> */}
+      {/* <List>
         {['All mail', 'Trash', 'Spam'].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
@@ -44,13 +51,16 @@ export default function TemporaryDrawer() {
             </ListItemButton>
           </ListItem>
         ))}
-      </List>
+      </List> */}
     </Box>
   );
 
   return (
-    <div className='absolute left-3 top-28 z-10'>
-      <Button onClick={toggleDrawer(true)}> <img src="src/assets/menu.svg" alt="" /></Button>
+    <div className="absolute left-3 top-28 z-10">
+      <Button onClick={toggleDrawer(true)}>
+        {" "}
+        <img src="src/assets/menu.svg" alt="" />
+      </Button>
       <Drawer open={open} onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
