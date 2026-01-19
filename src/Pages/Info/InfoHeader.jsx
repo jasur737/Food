44f9a813fc960e13661542139ea37155de7df648
jasur737 <baxtiyorovjasur728@gmail.com>
2026-01-19 19:51:35 +0500

    :)

--- src/Pages/Info/InfoHeader.jsx
+++ src/Pages/Info/InfoHeader.jsx
@@ -1,26 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 export default function InfoHeader() {
   return (
-    <section className='container m-auto'>
-         <div className='flex justify-end gap-10 pr-13 pt-15'>
-        <Link to="/" className='text-[#333333] after'>Завтраки </Link>
-        <Link to="/" className='text-[#333333]'>Raw</Link>
-        <Link to="/" className='text-[#333333]'>Холодные закуски</Link>
-        <Link to="/" className='text-[#333333]'>Брускетты</Link>
-        <Link to="/" className='text-[#333333]'>К вину </Link>
-        <Link to="/" className='text-[#333333]'>Салаты</Link>
-        <Link to="/" className='text-[#333333]'>Супы</Link>
-        <Link to="/" className='text-[#333333]'>Паста и ризотто </Link>
-        <Link to="/" className='text-[#333333]'>Мясо</Link>
-        <Link to="/" className='text-[#333333]'>Рыба </Link>
-        <Link to="/" className='text-[#333333]'>Grill </Link>
-        <Link to="/" className='text-[#333333]'>Мангал </Link>
-        <Link to="/" className='text-[#333333]'>Гарниры</Link>
-    </div>
-    <div>
-
-    </div>
+    <section className="container m-auto">
+      <div className="flex justify-end gap-10 pr-13 pt-15">
+        <Link to="/" className="text-[#333333] after">
+          Завтраки{" "}
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Raw
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Холодные закуски
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Брускетты
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          К вину{" "}
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Салаты
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Супы
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Паста и ризотто{" "}
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Мясо
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Рыба{" "}
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Grill{" "}
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Мангал{" "}
+        </Link>
+        <Link to="/" className="text-[#333333] after">
+          Гарниры
+        </Link>
+      </div>
+      <div className="mt-[60px]">
+        <div className="ml-[412px]">
+          <img
+            className="w-[416px] h-[317px]"
+            src="/src/assets/food1.png"
+            alt=""
+          />
+          <div className="flex gap-6 pt-6">
+            <img
+              className="w-[122px] h-[93px]"
+              src="/src/assets/food1.png"
+              alt=""
+            />
+            <img
+              className="w-[122px] h-[93px]"
+              src="/src/assets/food1.png"
+              alt=""
+            />
+            <img
+              className="w-[122px] h-[93px]"
+              src="/src/assets/food1.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div>
+          <p>Яичница-глазунья из трех яиц</p>
+          <div className="bg-[#333333] w-[306px] border border-[#333333]"></div>
+          <p><span>250 ₽</span> / 400 грамм</p>
+          <div>
+            <button></button>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
--- src/Pages/Menu/MenuHeader.jsx
+++ src/Pages/Menu/MenuHeader.jsx
@@ -4,19 +4,19 @@ export default function MenuHeader() {
   return (
    <section className='container m-auto mb-[150px]'>
     <div className='flex justify-end gap-10 pr-13 pt-15'>
-        <Link to="/" className='text-[#333333]'>Завтраки </Link>
-        <Link to="/" className='text-[#333333]'>Raw</Link>
-        <Link to="/" className='text-[#333333]'>Холодные закуски</Link>
-        <Link to="/" className='text-[#333333]'>Брускетты</Link>
-        <Link to="/" className='text-[#333333]'>К вину </Link>
-        <Link to="/" className='text-[#333333]'>Салаты</Link>
-        <Link to="/" className='text-[#333333]'>Супы</Link>
-        <Link to="/" className='text-[#333333]'>Паста и ризотто </Link>
-        <Link to="/" className='text-[#333333]'>Мясо</Link>
-        <Link to="/" className='text-[#333333]'>Рыба </Link>
-        <Link to="/" className='text-[#333333]'>Grill </Link>
-        <Link to="/" className='text-[#333333]'>Мангал </Link>
-        <Link to="/" className='text-[#333333]'>Гарниры</Link>
+        <Link to="/" className='text-[#333333] after'>Завтраки </Link>
+        <Link to="/" className='text-[#333333] after'>Raw</Link>
+        <Link to="/" className='text-[#333333] after'>Холодные закуски</Link>
+        <Link to="/" className='text-[#333333] after'>Брускетты</Link>
+        <Link to="/" className='text-[#333333] after'>К вину </Link>
+        <Link to="/" className='text-[#333333] after'>Салаты</Link>
+        <Link to="/" className='text-[#333333] after'>Супы</Link>
+        <Link to="/" className='text-[#333333] after'>Паста и ризотто </Link>
+        <Link to="/" className='text-[#333333] after'>Мясо</Link>
+        <Link to="/" className='text-[#333333] after'>Рыба </Link>
+        <Link to="/" className='text-[#333333] after'>Grill </Link>
+        <Link to="/" className='text-[#333333] after'>Мангал </Link>
+        <Link to="/" className='text-[#333333] after'>Гарниры</Link>
     </div>
     <div className='ml-[200px] mt-15 flex flex-wrap gap-6'>
        <Link to="Info" className='hover'>
