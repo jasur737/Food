9f8937fc1d805a2679494620d5e118838fb23f2c
jasur737 <baxtiyorovjasur728@gmail.com>
2026-01-20 16:40:54 +0500

    :)

--- src/Pages/Info/InfoHeader.jsx
+++ src/Pages/Info/InfoHeader.jsx
@@ -44,7 +44,7 @@ export default function InfoHeader() {
           Гарниры
         </Link>
       </div>
-      <div className="mt-[60px]">
+      <div className="mt-[60px] flex gap-[134px]">
         <div className="ml-[412px]">
           <img
             className="w-[416px] h-[317px]"
@@ -69,12 +69,47 @@ export default function InfoHeader() {
             />
           </div>
         </div>
-        <div>
+        <div className="flex flex-col gap-8">
           <p>Яичница-глазунья из трех яиц</p>
           <div className="bg-[#333333] w-[306px] border border-[#333333]"></div>
-          <p><span>250 ₽</span> / 400 грамм</p>
+          <p>
+            <span>250 ₽</span> / 400 грамм
+          </p>
           <div>
-            <button></button>
+            <div className="border border-[#a6a6a6] w-[84px] h-[58px] flex items-center justify-center gap-3">
+              <p>1</p>
+             <div className="flex flex-col gap-1.5">
+             <img src="/src/assets/up.svg" alt="" />
+             <img src="/src/assets/down.svg" alt="" />
+             </div>
+            </div>
+            <button>В корзину</button>
+          </div>
+          <div className="bg-[#333333] w-[306px] border border-[#333333]"></div>
+          <p className="font-bold text-[#333333] text-[32px]">Сделать еще вкуснее</p>
+          <div>
+            <form>
+            <label className="flex gap-7.5">
+              <p className="font-bold text-[#a6a6a6]">Тигровые креветки 60 г <br /> <span className="font-bold text-xl text-[#333333]">450 ₽</span></p>
+            
+              <input className="w-6 h-6 border border-[#a6a6a6]" type="checkbox" />
+            </label>
+            <label className="flex gap-7.5">
+              <p className="font-bold text-[#a6a6a6]">Томаты / паприка <br /> <span className="font-bold text-xl text-[#333333]">80 ₽</span></p>
+            
+              <input className="w-6 h-6 border border-[#a6a6a6]" type="checkbox" />
+            </label>
+            <label className="flex gap-7.5">
+              <p className="font-bold text-[#a6a6a6]">Бекон <br /> <span className="font-bold text-xl text-[#333333]">120 ₽</span></p>
+            
+              <input className="w-6 h-6 border border-[#a6a6a6]" type="checkbox" />
+            </label>
+            <label className="flex gap-7.5">
+              <p className="font-bold text-[#a6a6a6]">Сыр чеддер 30 г <br /> <span className="font-bold text-xl text-[#333333]">80 ₽</span></p>
+            
+              <input className="w-6 h-6 border border-[#a6a6a6]" type="checkbox" />
+            </label>
+            </form>
           </div>
         </div>
       </div>
